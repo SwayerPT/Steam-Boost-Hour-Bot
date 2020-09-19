2afc8004c53e87a4533aeab376331f6eec8cdc01
Swayer <31244474+SwayerPT@users.noreply.github.com>
2020-09-19 13:57:53 +0100

    Update appbot.js

--- appbot.js
+++ appbot.js
@@ -3,7 +3,7 @@
     Flow Community TeamSpeak: flowcm.ddns.net
     Created by Swayer
     
-    version.1.3.5
+    version.1.3.7 (no longer in development).
 */
 
 //===============//===============Variables process//
